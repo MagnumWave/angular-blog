ad1717003595b6b2e581f5ad7d472744d7da479d
MagnumWave <aureliomagno@terra.com.br>
2025-03-14 15:58:17 -0300

    dinamizando small card e big card

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+
+  @Input()
+  photoCover = "";
+  @Input()
+  cardTitle = "";
+  @Input()
+  cardDescription = "";
+
+  constructor(){}
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+
+    //meu scaffold gerou esse método com esse throw junto.
+    //esse throw estava interrompendo o ciclo de vida de renderizar os inputs passados.
+  }
+
 
 }
